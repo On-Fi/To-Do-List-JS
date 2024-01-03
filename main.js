343f8d20adb8d3a5657212faa5e9b6f8d2a0ea7e
onur fistikci <onurfistikci@mail.de>
2024-01-03 17:24:47 +0100

    Update main.js
    add a edit function

--- main.js
+++ main.js
@@ -8,14 +8,26 @@ function addTask() {
         newTask.innerHTML = `
             <span>${taskInput.value}</span>
             <button onclick="completeTask(this)">Complete</button>
+            <button onclick="editTask(this)">Edit</button>
         `;
         taskList.appendChild(newTask);
         taskInput.value = '';
     }
 }
 
-// tick task function
+// tick a task function
 function completeTask(button) {
     const task = button.parentElement;
     task.classList.toggle('completed');
+}
+
+// edit a task fuction
+function editTask(button) {
+    const task = button.parentElement;
+    const span = task.querySelector('span');
+    const newTask = prompt('Edit task:', span.textContent);
+
+    if (newTask !== null) {
+        span.textContent = newTask;
+    }
 }
